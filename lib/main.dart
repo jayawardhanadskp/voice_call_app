e5f5f6a15f3d27ccc94f01bd7b03053dedd640d6
jayawardhanadskp <kasunpradeep.ds@gmail.com>
2024-06-13 23:48:46 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -16,29 +16,7 @@ void main() async {
 
   FirebaseMessaging messaging = FirebaseMessaging.instance;
 
-  // Request permission on iOS
-  NotificationSettings settings = await messaging.requestPermission(
-    alert: true,
-    announcement: false,
-    badge: true,
-    carPlay: false,
-    criticalAlert: false,
-    provisional: false,
-    sound: true,
-  );
 
-  await FirebaseMessaging.instance.requestPermission();
-  await FirebaseMessaging.instance.getInitialMessage();
-
-  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
-    alert: true,
-    badge: true,
-    sound: true,
-  );
-
-  // Get the token
-  String? token = await messaging.getToken();
-  print("FCM Token: $token");
 
   final agoraService = AgoraService();
   await agoraService.initializeAgora();
