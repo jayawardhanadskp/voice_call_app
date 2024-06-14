cf72d6b49474025227cb11adc17f490d81ad00e7
jayawardhanadskp <kasunpradeep.ds@gmail.com>
2024-06-14 16:20:40 +0530

    user model

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -5,6 +5,14 @@ class UserModel {
 
   UserModel({required this.id, required this.name, this.token});
 
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'],
+      name: json['name'],
+        token : json['token']
+    );
+  }
+
   Map<String, dynamic> toJson() {
     return {
       'id': id,
