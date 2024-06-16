b5914789e99055cf0175079f6fcb060004c468e5
jayawardhanadskp <kasunpradeep.ds@gmail.com>
2024-06-16 18:29:37 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,131 +1,38 @@
-import 'dart:convert';
 
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:voice_call_app/services/agora_service.dart';
+
+import 'services/firebase_helper.dart';
+import 'services/notification_service.dart';
 
 
-import 'firebase_options.dart';
-import 'model/user_model.dart';
-import 'screens/call_screen.dart';
-import 'screens/ongoing_call_screen.dart';
-import 'screens/user_selection_screen.dart';
-import 'services/agora_service.dart';
-import 'services/firebase_messaging_service.dart';
+late final Widget screen;
 
-void main() async {
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+Future main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  FirebaseMessaging messaging = FirebaseMessaging.instance;
-  
-
-  // Request permission on iOS
-  NotificationSettings settings = await messaging.requestPermission(
-    alert: true,
-    announcement: false,
-    badge: true,
-    carPlay: false,
-    criticalAlert: false,
-    provisional: false,
-    sound: true,
-  );
-
-  await FirebaseMessaging.instance.requestPermission();
-  await FirebaseMessaging.instance.getInitialMessage();
-
-  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
-    alert: true,
-    badge: true,
-    sound: true,
-  );
-
-  // Get the token
-  String? token = await messaging.getToken();
-  print("FCM Token: $token");
+  await FirebaseHelper.setupFirebase();
+  await NotificationService.initializeNotification();
 
   final agoraService = AgoraService();
   await agoraService.initializeAgora();
 
 
+  screen = FirebaseHelper.homeScreen;
+
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      RemoteNotification? notification = message.notification;
-      AndroidNotification? android = message.notification?.android;
-
-      if (notification != null && android != null) {
-        showDialog(
-          context: context,
-          builder: (_) {
-            return AlertDialog(
-              title: Text(notification.title ?? ''),
-              content: Text(notification.body ?? ''),
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    _handleAction('ANSWER_CALL', message.data);
-                  },
-                  child: Text('Answer'),
-                ),
-                TextButton(
-                  onPressed: () {
-                    _handleAction('REJECT_CALL', message.data);
-                  },
-                  child: Text('Reject'),
-                ),
-              ],
-            );
-          },
-        );
-      }
-    });
-
-    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      _handleAction(message.data['action'], message.data);
-    });
-  }
-
-  void _handleAction(String? action, Map<String, dynamic> data) {
-    if (action == 'ANSWER_CALL') {
-      // Parse user data from the notification payload
-      UserModel user = UserModel.fromJson(jsonDecode(data['user']));
-      String token = data['token'];
-
-      // Navigate to CallScreen
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => CallScreen(user: user, token: token),
-        ),
-      );
-    } else if (action == 'REJECT_CALL') {
-      // Handle call rejection
-      print('Call rejected');
-    }
-  }
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Voice Call App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: UserListScreen(),
+      debugShowCheckedModeBanner: false,
+      navigatorKey: navigatorKey,
+      home: screen,
     );
   }
 }
