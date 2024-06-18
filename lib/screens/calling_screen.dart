abfb14289d9f1b4d2f5ff20de04e8b810e34fda7
jayawardhanadskp <kasunpradeep.ds@gmail.com>
2024-06-18 22:53:15 +0530

    calling screen

--- lib/screens/calling_screen.dart
+++ lib/screens/calling_screen.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 
+import '../services/agora_service.dart';
+import '../services/firebase_helper.dart';
+import 'ongoing_call_screen.dart';
+
 class CallingScreen extends StatefulWidget {
 
+  final String callerName;
+  final String channelName;
   final String selectUser;
+  final agoraService;
+
   const CallingScreen({
     Key? key,
-    required this.selectUser
+    required this.selectUser,
+    required this.callerName,
+    required this.channelName,
+    required this.agoraService,
   }): super(key: key);
 
   @override
@@ -13,6 +24,41 @@ class CallingScreen extends StatefulWidget {
 }
 
 class _CallingScreenState extends State<CallingScreen> {
+
+  final AgoraService _agoraService = AgoraService();
+
+  @override
+  void initState() {
+    super.initState();
+
+
+    _initAgora();
+    _listenToCallStatus();
+  }
+
+  Future<void> _initAgora() async {
+    await _agoraService.initializeAgora();
+  }
+
+  void _listenToCallStatus() {
+    CallStatus.getCallStatusStream(widget.channelName).listen((snapshot) {
+      if (snapshot.exists && snapshot.data()?['status'] == 'ongoing') {
+        // Navigate to OngoingCallScreen when call status is 'ongoing'
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => OngoingCallScreen(
+              channelName: widget.channelName,
+              callerName: widget.callerName,
+              agoraService: _agoraService,
+            ),
+          ),
+        );
+      }
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
